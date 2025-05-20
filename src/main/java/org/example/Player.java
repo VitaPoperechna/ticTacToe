dd1ad13d00c0d8ec1e16b2824bb37c5283ca3c98
VitaPoperechna <vitapoperechna@gmal.com>
2025-05-20 12:04:20 +0200

    feat: add player class

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Player {
+    private char firstPlayer = 'X';
+    private char secondPlayer = 'O';
+
+    public char getSecondPlayer() {
+        return secondPlayer;
+    }
+
+    public char getFirstPlayer() {
+        return firstPlayer;
+    }
+}
+
