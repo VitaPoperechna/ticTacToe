ce732b023f4cda0dc0e22866d264905610ed7126
Lara Pla <laraplamoreno@gmail.com>
2025-05-21 18:39:01 +0200

    refactor: add player method to change turn

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -10,16 +10,15 @@ public class Game {
     public void game(){
         System.out.println("¡Vamos a jugar a Tic Tac Toe!\n" + "El tablero se compone de tres columnas: 0-1-2 y tres filas: 0-1-2");
         board.showBoard();
- 
-        System.out.println("\nTurno de: " + player.getFirstPlayer());
-        System.out.print( "Escribe primero una fila y seguido una columna con un espacio en medio (ej. 1 1): ");
 
+        System.out.println("\nTurno de: " + player.getPlayer()[player.getCurrentIndex()]);
+        System.out.print( "Escribe primero una fila y seguido una columna con un espacio en medio (ej. 1 1): ");
 
         int row = scanner.nextInt();
         int column = scanner.nextInt();
         if ((row >= 0 && row <= 2) && (column >= 0 && column <= 2)) {
             if (board.getBoard()[row][column] == '_') {
-                board.getBoard()[row][column] = firstPlayer.getPlayer();
+                board.getBoard()[row][column] = player.getPlayer()[player.getCurrentIndex()];
                 board.showBoard();
             } else {
                 System.out.println("En el espacio que has elegido ya hay una ficha");
@@ -27,5 +26,7 @@ public class Game {
         } else {
             System.out.println("Algún número que has escrito no está en rango");
         }
+        row = scanner.nextInt();
+        column = scanner.nextInt();
     }
 }
